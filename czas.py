d819d70e2ac1f32f4c1661725b4605aa2bf5fba2
kjakubowskaa <k244523@gmail.com>
2022-12-10 13:11:41 +0100

    git commit

---
+++ czas.py
@@ -0,0 +1,36 @@
+#import os
+
+
+#os.system('cmd c "cd C/:Users\\vdi-student\\projectwsb"')
+#os.system('cmd c "dir /s new.txt >> result.txt"')
+
+#os.system('cmd /c "cd C://Users//vdi-student//projectwsb && dir /s new.txt >> results.txt"')
+
+
+#my_string1 = 'mom'
+#my_string2 = 'dad'
+
+#sum1 = my_string1 + my_string2
+#print(sum1)
+
+#sum2 = 'my ' + my_string1 + 'and my ' + my_string2
+#print(sum2)
+
+#print(sum2[3:6])
+#print(sum2[-3:])
+
+import datetime
+today = datetime.date.today()
+print(type(today))
+print(today)
+
+
+
+
+
+
+
+
+
+
+
